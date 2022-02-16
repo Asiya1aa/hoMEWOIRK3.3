b994d3697d8d1d5300645b1d6a97cd24f5b22215
Asiya76 <asiyaasanalieva1234@gmail.com>
2022-02-16 20:31:30 +0600

    Initial commit

---
+++ app/src/main/java/com/example/homewoirk33/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.homewoirk33;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportFragmentManager().beginTransaction().add(R.id.container, new com.example.app.OneFragment()).commit();
+    }
+}package com.example.app;
+        import androidx.appcompat.app.AppCompatActivity;
+        import android.os.Bundle;
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportFragmentManager().beginTransaction().add(R.id.container, new com.example.app.OneFragment()).commit();
+    }
+}
